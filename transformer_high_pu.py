4548a4dbbb959213a02178e9016c98215a3cc9e3
Tony Xiang <tony.xiang@alliander.com>
2025-01-14 15:23:33 +0100

    initial file
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

---
+++ transformer_high_pu.py
@@ -0,0 +1,25 @@
+import pandapower as pp
+
+
+net = pp.create_empty_network()
+pp.create_bus(net, vn_kv=10.0, index=0)
+pp.create_bus(net, vn_kv=10.0, index=1)
+pp.create_transformer_from_parameters(
+    net,
+    hv_bus=0,
+    lv_bus=1,
+    sn_mva=100.0,
+    vk_percent=10,
+    vkr_percent=0.5,
+    vn_hv_kv=10.0,
+    vn_lv_kv=25.0,  # parametrize vn_lv_kv
+    pfe_kw=0.0,
+    i0_percent=0.5,
+)
+pp.create_ext_grid(net, bus=0, vm_pu=1.0, va_degree=0.0)  # parametrize vm_pu
+pp.create_load(net, bus=1, p_mw=1.0, q_mvar=0.0)  # parametric p_mw, q_mvar
+
+pp.runpp(net, algorithm="nr", init="auto", init_vm_pu=[1.0, 1.5])  # parametrize init_vm_pu
+
+print(net.res_bus)
+print(net.res_trafo)
