3e3c750dfa9aa2f8a1cebde617e3e7f9df63e72b
Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>
2025-01-17 17:06:21 +0100

    added missing __init__ and updated README
    Signed-off-by: Jerry Guo <Jerry.Jinfeng.Guo@alliander.com>

---
+++ data/complete_net/__init__.py
@@ -0,0 +1,7 @@
+"""
+Complete networks
+"""
+
+# complete networks with 2 buses
+from .high_generation_segmented_grid import HighGenSegmentedNet
+from .transformer_high_pu_segmented_grid import TrafoHighPuSegmentedNet
